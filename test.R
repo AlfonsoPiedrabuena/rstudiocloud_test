784c7a55215b780fef3ccbe850ac03a03a78761b
Alfonso <alfonso.piedrabuena1@gmail.com>
2021-05-22 18:53:34 +0000

    2º prueba con conexión a base de datos

--- test.R
+++ test.R
@@ -1,2 +1,29 @@
-x<- rnorm(1000)
-hist(x)
+library(DBI)
+library(RMySQL)
+
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest")
+
+dbListTables(MyDataBase)
+dbListFields(MyDataBase, 'City')
+DataDB <- dbGetQuery(MyDataBase, "select * from City")
+class(DataDB)
+dim(DataDB)
+head(DataDB)
+
+pop.mean <- mean(DataDB$Population)  # Media a la variable de población
+pop.mean 
+
+pop.3 <- pop.mean *3   # Operaciones aritméticas
+pop.3
+
+library(dplyr)
+pop50.mex <-  DataDB %>% filter(CountryCode == "MEX" ,  Population > 50000)   # Ciudades del país de México con más de 50,000 habitantes
+
+head(pop50.mex)
+
+unique(DataDB$CountryCode)   # Países que contiene la BDD
